dd2f1ed03f0b42ad543394d37ed73829a9ad0da7
duckvector <duckvector@gmail.com>
2026-08-22 14:26:51 +0530

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,272 @@
+-- ====================
+-- 1. BASIC SETTINGS & NETRW STYLING
+-- ====================
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+
+-- Netrw Styling
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 0
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 25
+
+-- ====================
+-- 2. KEYMAPS
+-- ====================
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Explorer" })
+
+vim.keymap.set("n", "<leader>x", ":ToggleTerm<CR>", { desc = "Terminal" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Global LSP Attach Autocmd for Keymaps
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+  end,
+})
+
+-- ====================
+-- 3. BOOTSTRAP LAZY.NVIM
+-- ====================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ====================
+-- 4. PLUGINS
+-- ====================
+require("lazy").setup({
+
+  -- Colorscheme: One Dark
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({ style = "dark" })
+      require("onedark").load()
+    end,
+  },
+
+  -- Statusline: Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "onedark",
+          component_separators = "|",
+          section_separators = "",
+        },
+      })
+    end,
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+},
+
+  -- Dashboard
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      dashboard.section.buttons.val = {
+        dashboard.button("f", "🔍  Find file", "<cmd>Telescope find_files<cr>"),
+        dashboard.button("e", "📂  Explore files", "<cmd>Ex<cr>"),
+        dashboard.button("r", "🕒  Recent files", "<cmd>Telescope oldfiles<cr>"),
+        dashboard.button("q", "❌  Quit", "<cmd>qa<cr>"),
+      }
+      alpha.setup(dashboard.opts)
+    end,
+  },
+
+  -- Auto Save
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup({
+        enabled = true,
+        execution_message = { message = function() return "" end },
+        trigger_events = { "InsertLeave", "TextChanged" },
+      })
+    end,
+  },
+
+  -- Split Terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 15,
+        open_mapping = [[<C-\>]],
+        hide_numbers = true,
+        shade_terminals = true,
+        direction = "horizontal",
+        close_on_exit = true,
+        shell = vim.o.shell,
+      })
+
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+    end,
+  },
+
+  -- Autocompletion Engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
+  -- LSP Management (Mason & Native Neovim 0.11 Configs)
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "lua_ls"},
+      })
+
+      -- Native Neovim 0.11+ LSP setup
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("ts_ls")
+
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = { diagnostics = { globals = { "vim" } } },
+        },
+      })
+      vim.lsp.enable("lua_ls")
+    end,
+  },
+
+  -- Syntax Highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "python" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  -- Fuzzy Finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find text" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    },
+  },
+
+  -- Keymap Helper
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+  },
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+        view = {
+            side = "left"
+        },
+    },
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+    },
+})
+
